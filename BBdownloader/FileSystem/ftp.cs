4981d5889a13b36fb64a64a38ad6a28487f243f0
Jozef <jozef.rudy@gmail.com>
2015-11-02 15:33:38 +0100

    ftp refactoring

--- BBdownloader/FileSystem/ftp.cs
+++ BBdownloader/FileSystem/ftp.cs
@@ -25,95 +25,6 @@ namespace BBdownloader.FileSystem
             pass = password;
         }
 
-        /* Download File */
-        
-        public void download(string remoteFile, string localFile)
-        {
-            try
-            {
-                /* Create an FTP Request */
-                ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + remoteFile);
-                /* Log in to the FTP Server with the User Name and Password Provided */
-                ftpRequest.Credentials = new NetworkCredential(user, pass);
-                /* When in doubt, use these options */
-                ftpRequest.UseBinary = true;
-                ftpRequest.UsePassive = true;
-                ftpRequest.KeepAlive = true;
-                /* Specify the Type of FTP Request */
-                ftpRequest.Method = WebRequestMethods.Ftp.DownloadFile;
-                /* Establish Return Communication with the FTP Server */
-                ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
-                /* Get the FTP Server's Response Stream */
-                ftpStream = ftpResponse.GetResponseStream();
-                /* Open a File Stream to Write the Downloaded File */
-                
-                
-                FileStream localFileStream = new FileStream(localFile, FileMode.Create);
-                /* Buffer for the Downloaded Data */
-                byte[] byteBuffer = new byte[bufferSize];
-                int bytesRead = ftpStream.Read(byteBuffer, 0, bufferSize);                
-
-                /* Download the File by Writing the Buffered Data Until the Transfer is Complete */
-                try
-                {
-                    while (bytesRead > 0)
-                    {                        
-                        localFileStream.Write(byteBuffer, 0, bytesRead);
-                        bytesRead = ftpStream.Read(byteBuffer, 0, bufferSize);
-                    }
-                }
-                catch (Exception ex) { Console.WriteLine(ex.ToString()); }
-                /* Resource Cleanup */
-                localFileStream.Close();
-                ftpStream.Close();
-                ftpResponse.Close();
-                ftpRequest = null;
-            }
-            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
-            return;
-        }
-
-        /* Upload File */
-        public void upload(string remoteFile, string localFile)
-        {
-            try
-            {
-                /* Create an FTP Request */
-                ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + remoteFile);
-                /* Log in to the FTP Server with the User Name and Password Provided */
-                ftpRequest.Credentials = new NetworkCredential(user, pass);
-                /* When in doubt, use these options */
-                ftpRequest.UseBinary = true;
-                ftpRequest.UsePassive = true;
-                ftpRequest.KeepAlive = true;
-                /* Specify the Type of FTP Request */
-                ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
-                /* Establish Return Communication with the FTP Server */
-                ftpStream = ftpRequest.GetRequestStream();
-                /* Open a File Stream to Read the File for Upload */
-                FileStream localFileStream = new FileStream(localFile, FileMode.Create);
-                /* Buffer for the Downloaded Data */
-                byte[] byteBuffer = new byte[bufferSize];
-                int bytesSent = localFileStream.Read(byteBuffer, 0, bufferSize);
-                /* Upload the File by Sending the Buffered Data Until the Transfer is Complete */
-                try
-                {
-                    while (bytesSent != 0)
-                    {
-                        ftpStream.Write(byteBuffer, 0, bytesSent);
-                        bytesSent = localFileStream.Read(byteBuffer, 0, bufferSize);
-                    }
-                }
-                catch (Exception ex) { Console.WriteLine(ex.ToString()); }
-                /* Resource Cleanup */
-                localFileStream.Close();
-                ftpStream.Close();
-                ftpRequest = null;
-            }
-            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
-            return;
-        }
-
         /* Rename File */
         public void rename(string currentFileNameAndPath, string newFileName)
         {
@@ -143,6 +54,10 @@ namespace BBdownloader.FileSystem
 
         public string[] ReadFile(string remoteFile)
         {
+            if (!this.DirectoryExists(remoteFile))
+                return new string[] {""};
+
+
             var contents = new List<string>();
             try
             {
@@ -266,6 +181,9 @@ namespace BBdownloader.FileSystem
         /* Delete File */
         public bool DeleteFile(string deleteFile)
         {
+            if (!this.DirectoryExists(deleteFile))
+                return false;
+
             try
             {
                 /* Create an FTP Request */
@@ -294,6 +212,9 @@ namespace BBdownloader.FileSystem
         /* List Directory Contents File/Folder Name Only */
         public string[] ListFiles(string directory)
         {
+            if (!this.DirectoryExists(directory))
+                return new string[] { "" };
+
             try
             {
                 /* Create an FTP Request */
@@ -532,7 +453,7 @@ namespace BBdownloader.FileSystem
 
         public bool FileExists(string path)
         {
-            throw new NotImplementedException();
+            return this.DirectoryExists(path);           
         }
 
     }
