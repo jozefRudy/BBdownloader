8c690693283bc680857004ed91ee0329b0821c72
jozefRudy <jozef.rudy@gmail.com>
2017-04-19 09:35:26 -0400

    delete installation files

--- Installation/deploy.bat
+++
@@ -1,3 +0,0 @@
-c:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ..\BBdownloader\BBdownloader.csproj /t:Clean;Build /p:Configuration=Release /p:Platform="AnyCPU"
-"c:\Program Files (x86)\Inno Setup 5\compil32.exe" /cc setup.iss
-PAUSE
